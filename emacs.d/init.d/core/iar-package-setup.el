9099418ad1a3758aab307a74704258438298de07
emacboros <emacboros@proton.me>
2026-08-09 14:58:20 +0000

    Add mcp to auto-install list, create pentesting project file
    - iar-package-setup.el: auto-install mcp package from MELPA
    - personalization/projects/pentesting.org: pentesting project with
      #+CONTAINERS: pentest and #+MCP: burp

--- emacs.d/init.d/core/iar-package-setup.el
+++ emacs.d/init.d/core/iar-package-setup.el
@@ -6,4 +6,14 @@
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
+
+(unless (package-installed-p 'gptel)
+  (package-install 'gptel))
+
+(unless (package-installed-p 'undercover)
+  (package-install 'undercover))
+
+(unless (package-installed-p 'mcp)
+  (package-install 'mcp))
+
 (provide 'iar-package-setup)
